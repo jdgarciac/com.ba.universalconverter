3189389ee2189876ba19d3ba60cab2ab396446b9
Juan Garcia <juandavidgarcia@outlook.com>
2021-03-01 14:09:48 -0500

    se modifica task

--- src/main/java/tasks/ConvertLengthUnits.java
+++ src/main/java/tasks/ConvertLengthUnits.java
@@ -1,7 +1,10 @@
 package tasks;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileDriver;
+import io.appium.java_client.PerformsTouchActions;
 import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
 import models.LengthDataModel;
@@ -11,11 +14,13 @@ import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import userinterfaces.LengthDataPage;
 
 import java.time.Duration;
 import java.util.List;
 
+import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getDriver;
 import static net.thucydides.core.webdriver.ThucydidesWebDriverSupport.getProxiedDriver;
 
 public class ConvertLengthUnits implements Task {
@@ -34,12 +39,12 @@ public class ConvertLengthUnits implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
 
-        WebDriver driver = getProxiedDriver();
+        AndroidDriver<WebElement> driver = getProxiedDriver();
         Dimension dimension = driver.manage().window().getSize();
         int xPosition = dimension.width/2;
         int initialPositionY = dimension.height/2;
         int finalPositionY = dimension.height/4;
-        TouchAction action = new TouchAction((AppiumDriver) driver);
+        TouchAction action = new TouchAction(driver);
 
         actor.attemptsTo(
                 Click.on(LengthDataPage.UNIT_FROM)
